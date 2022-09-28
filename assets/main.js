60feef76b63768b86190a2a9af2666bf22ed9f70
DamianRondinon <damianrondinon@gmail.com>
2022-09-28 15:48:52 -0300

    Frontend finalizado

--- assets/main.js
+++ assets/main.js
@@ -31,9 +31,19 @@ window.onload = () =>{
     } else{
         document.querySelector('.header .header-2').classList.remove('active')
     }
+
+    fadeOut();
 }
 
-var swiper = new Swiper(".featured-slider", {
+function loader(){
+  document.querySelector(".loader-container").classList.add("active");
+}
+
+function fadeOut(){
+  setTimeout(loader, 4000);
+}
+
+var swiper = new Swiper(".books-slider", {
     loop:true,
     centeredSlides: true,
     autoplay: {
@@ -68,6 +78,10 @@ var swiper = new Swiper(".featured-slider", {
         delay: 9500,
         disableOnInteraction: false,
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
       0: {
         slidesPerView: 1,
@@ -80,6 +94,71 @@ var swiper = new Swiper(".featured-slider", {
       },
       1024: {
         slidesPerView: 4,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
       },
     },
   });
